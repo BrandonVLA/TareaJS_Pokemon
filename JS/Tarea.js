6c0a2bac9053cc0f1748999d7eb1571c22835525
BrandonVLA <bbarrantescalderon@vlacademy.com>
2025-01-30 18:06:48 -0600

    Se cambio la manera de mostrar el navbar de html a JS.

--- JS/Tarea.js
+++ JS/Tarea.js
@@ -94,6 +94,41 @@ Mostrar_Pokemon_Posicion8();
 //   `;
 // }
 
+
+document.getElementById("Id_Navbar").innerHTML = 
+`
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <a class="navbar-brand" href="/Tarea.html">POKIDex</a>
+      <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+        aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link " href="#" onclick="DarkMode('black','black')">Modo Oscuro <span
+                class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" onclick="LightMode('white','black')">Modo Claro </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true"
+              aria-expanded="false">Mas</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownId">
+              <a class="dropdown-item" href="#">Proximamente...</a>
+              <a class="dropdown-item" href="#">Proximamente...</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+`
+
 //----- Inicio - Funciones Mostrar_Pokemon_Posicion-----//
 
 function Mostrar_Pokemon_Posicion0() {
